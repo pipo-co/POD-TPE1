873bd20c3590ecff61da9748ae8583dab778182a
Tobias Brandy <tbrandy@itba.edu.ar>
2021-09-08 21:32:30 -0300

    Fix FlightTrackingClient: Exportamos y des-exportamos el callback

--- client/src/main/java/ar/edu/itba/pod/client/clients/FlightTrackingClient.java
+++ client/src/main/java/ar/edu/itba/pod/client/clients/FlightTrackingClient.java
@@ -1,12 +1,15 @@
 package ar.edu.itba.pod.client.clients;
 
+import ar.edu.itba.pod.callbacks.FlightRunwayEventConsumer;
 import ar.edu.itba.pod.interfaces.FlightTrackingService;
+import ar.edu.itba.pod.models.FlightRunwayEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 
 import static ar.edu.itba.pod.client.ClientUtils.*;
 
@@ -22,9 +25,9 @@ public final class FlightTrackingClient {
 
         final Registry registry = getRegistry(System.getProperty("serverAddress", DEFAULT_REGISTRY_ADDRESS));
 
-        final String airline = System.getProperty("airline");
+        final String airline = System.getProperty("airlineName");
         if(airline == null) {
-            throw new IllegalArgumentException("Argument 'airline' is mandatory");
+            throw new IllegalArgumentException("Argument 'airlineName' is mandatory");
         }
 
         final String flightCode = System.getProperty("flightCode");
@@ -34,31 +37,45 @@ public final class FlightTrackingClient {
 
         final FlightTrackingService service = (FlightTrackingService) registry.lookup(FlightTrackingService.CANONICAL_NAME);
 
-        service.suscribeToFlight(airline, flightCode, e -> {
-            switch(e.getType()) {
-                case RUNWAY_ASSIGNMENT:
-                    System.out.println(
-                        "Flight " + e.getFlight()
-                        + " with destiny " + e.getDestination() + " was assigned to runway " + e.getRunway()
-                        + " and there are " + e.getPosition() + "flights waiting ahead."
-                    );
-                    break;
-                case RUNWAY_PROGRESS:
-                    System.out.println(
-                        "A flight departed from runway " + e.getRunway()
-                        + ". Flight " + e.getFlight() + " with destiny " + e.getDestination() + " has "
-                        + e.getPosition() + "flights waiting ahead."
-                    );
-                    break;
-                case FLIGHT_TAKE_OFF:
-                    System.out.println(
-                        "Flight " + e.getFlight() + " with destiny "
-                        + e.getDestination() + " departed on runway " + e.getRunway() + "."
-                    );
-                    break;
-            }
-        });
+        final FlightRunwayEventConsumer callback = new RunwayEventCallback();
+        UnicastRemoteObject.exportObject(callback, 0);
+
+        service.suscribeToFlight(airline, flightCode, callback);
 
         logger.info("Flight Tracking Client Ended");
+    }
+
+    private static final class RunwayEventCallback implements FlightRunwayEventConsumer {
+
+        @Override
+        public void accept(final FlightRunwayEvent e) throws RemoteException {
+            final String out;
+            switch(e.getType()) {
+                case RUNWAY_ASSIGNMENT:
+                    out = "Flight " + e.getFlight()
+                        + " with destiny " + e.getDestination() + " was assigned to runway " + e.getRunway()
+                        + " and there are " + e.getPosition() + " flights waiting ahead."
+                        ;
+                    break;
+                case RUNWAY_PROGRESS:
+                    out = "A flight departed from runway " + e.getRunway()
+                        + ". Flight " + e.getFlight() + " with destiny " + e.getDestination() + " has "
+                        + e.getPosition() + " flights waiting ahead."
+                        ;
+                    break;
+                case FLIGHT_TAKE_OFF:
+                    out ="Flight " + e.getFlight() + " with destiny "
+                        + e.getDestination() + " departed on runway " + e.getRunway() + "."
+                        ;
+                    // Como el avion ya despego, des-exportamos el callback
+                    // Como necesitamos la autoreferencia, no podemos usar lambda :(
+                    UnicastRemoteObject.unexportObject(this, true);
+                    break;
+                default:
+                    throw new IllegalStateException();
+            }
+
+            System.out.println(out);
+        }
     }
 }
