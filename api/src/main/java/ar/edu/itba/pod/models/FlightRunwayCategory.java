47177aa00b696cb969bbb5e2ee9a0f086f379883
Tobias Brandy <tbrandy@itba.edu.ar>
2021-09-08 23:32:17 -0300

    FlightRunwayCategory: values y size estatico

--- api/src/main/java/ar/edu/itba/pod/models/FlightRunwayCategory.java
+++ api/src/main/java/ar/edu/itba/pod/models/FlightRunwayCategory.java
@@ -1,5 +1,10 @@
 package ar.edu.itba.pod.models;
 
+import java.util.List;
+
 public enum FlightRunwayCategory {
-    A, B, C, D, E, F
+    A, B, C, D, E, F,
+    ;
+    public static final List<FlightRunwayCategory>  VALUES = List.of(values());
+    public static final int                         SIZE = VALUES.size();
 }
